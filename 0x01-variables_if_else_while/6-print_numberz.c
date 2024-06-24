f330f2965b12a149eb6b42212291d921eb899a96
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-06-24 11:56:11 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,13 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
 
 	putchar('\n');
 
