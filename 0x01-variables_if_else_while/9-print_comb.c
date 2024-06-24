35f80848420df3711910eb842ed55ead94e02fe0
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-06-24 12:07:36 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point, prints all possible combinations.
+ * main - Entry point, prints all single-digit numbers.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit1, digit2;
+	int digit;
 
-	for (digit1 = 0; digit1 <= 8; digit1++)
+	for (digit = 0; digit <= 9; digit++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
-		{
-			putchar(digit1 + '0');
-			putchar(digit2 + '0');
+		putchar(digit + '0');
 
-			if (digit1 != 8 || digit2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
