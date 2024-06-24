c644d0649d69d8fcfd46506cb85fc503ee0f9ce2
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-06-24 11:02:18 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@ int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str, 59);  // 59 is the length of the string including the null terminator
+	write(2, str, 59);
 
-	return 1;  // Return 1 to indicate an error occurred as per the requirement
+	return 1;  
 }
 
