49fed996486245dbc2eeb883618ad1bc2ce23e1b
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-06-24 11:51:11 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints all single digit numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+
+	putchar('\n');
+
+	return (0);
+}
+
