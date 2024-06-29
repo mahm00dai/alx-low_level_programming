6e58a297950b0a898eeb81a561d1947620a012d9
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-06-29 13:35:07 -0700

    Functions in C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ *
+ * Description: This function prints the alphabet from 'a' to 'z' using
+ *              the _putchar function. The entire alphabet and a newline
+ *              character are printed using only two calls to _putchar.
+ */
+void print_alphabet(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+
+	/* Use _putchar twice to print the entire alphabet and a new line */
+	while (alphabet[i] != '\0')
+	{
+		_putchar(alphabet[i]);
+		i++;
+	}
+}
+
+/**
  * main - Check the code for print_alphabet function
  *
  * Return: Always 0 (Success)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,22 +3,6 @@
 
 int _putchar(char c);
 void print_alphabet(void);
-/**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
- *
- * Description: This function prints the alphabet from 'a' to 'z' using
- *              the _putchar function. The entire alphabet and a newline
- *              character are printed using only two calls to _putchar.
- */
-void print_alphabet(void)
-{
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-	int i;
 
-	for (i = 0; alphabet[i] != '\0'; i++)
-	{
-		_putchar(alphabet[i]);
-	}
-}
 #endif /* MAIN_H */
 
