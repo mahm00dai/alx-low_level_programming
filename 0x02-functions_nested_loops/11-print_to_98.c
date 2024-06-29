6cdf939c5866f4d127df3d27cc28e647907bb116
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-06-29 15:38:11 -0700

    Functions in C

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,48 +3,37 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: starting number
+ *
+ * Description:
+ * Prints all numbers from n up to 98, separated by commas
+ * and ending with a newline, using _putchar instead of printf.
  */
 void print_to_98(int n)
 {
-	int i;
+	int i = n;
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; i < 98; i++)
 		{
-			if (i / 10 != 0)
-			{
-				_putchar('0' + i / 10);
-				_putchar('0' + i % 10);
-			}
-			else
-				_putchar('0' + i);
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (; i > 98; i--)
 		{
-			if (i / 10 != 0)
-			{
-				_putchar('0' + i / 10);
-				_putchar('0' + i % 10);
-			}
-			else
-				_putchar('0' + i);
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
 
