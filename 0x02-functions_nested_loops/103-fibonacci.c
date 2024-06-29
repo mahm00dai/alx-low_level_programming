e402b9ca684ff7b77ca3496d78073d8d1250fb29
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-06-29 16:09:09 -0700

    Functions in C

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,7 +12,7 @@
 int main(void)
 {
 	unsigned long first = 1, second = 2, next;
-	unsigned long long sum = 0; 
+	unsigned long long sum = 0;
 
 	while (second <= 4000000)
 	{
@@ -24,7 +24,7 @@ int main(void)
 		second = next;
 	}
 
-	printf(sum);
+	printf("", sum);
 
 	return (0);
 }
