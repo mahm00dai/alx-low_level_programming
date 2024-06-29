45ebdc7af993b851ec5d6d4c207230063daaf851
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-06-29 15:26:41 -0700

    Functions in C

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,9 +6,11 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n <= 98)
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i / 10 != 0)
 			{
@@ -27,7 +29,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i / 10 != 0)
 			{
@@ -44,6 +46,5 @@ void print_to_98(int n)
 			}
 		}
 	}
-	_putchar('\n');
 }
 
