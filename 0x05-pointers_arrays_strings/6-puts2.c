d32c14076123dedb6a4ec8c7f5ebfbf01b958ccf
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-01 05:28:14 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string,
+ * starting with the first character
  * @str: input string
  */
 void puts2(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != '\0')
-        {
-                _putchar(str[i]);
-                i += 2;  /* move to every other character */
-        }
-        _putchar('\n');  /* print a newline at the end */
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;  /* move to every other character */
+	}
+	_putchar('\n');  /* print a newline at the end */
 }
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,8 +7,8 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(char *s);  /* Prototype for rev_string */
-void puts2(char *str);  /* Prototype for puts2 */
+void rev_string(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */
 
