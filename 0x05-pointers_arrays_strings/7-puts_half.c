f6bcdd4d3b5893a8f74220def244dd3fa0f83611
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-01 05:38:04 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,23 +6,30 @@
  */
 void puts_half(char *str)
 {
+	/* Calculate the length of the string */
 	int length = 0;
 	int i, n;
 
-	/* Calculate the length of the string */
 	while (str[length] != '\0')
+	{
 		length++;
+	}
 
 	/* Calculate the starting index based on even or odd length */
 	if (length % 2 == 0)
+	{
 		n = length / 2;
+	}
 	else
+	{
 		n = (length - 1) / 2;
+	}
 
 	/* Print the second half of the string */
 	for (i = n; i < length; i++)
+	{
 		_putchar(str[i]);
-
+	}
 	_putchar('\n');  /* Print a newline at the end */
 }
 
