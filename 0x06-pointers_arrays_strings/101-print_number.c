d14a7c27cfd2d5eeb113cd87286179d7e6ab089b
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-04 07:11:56 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	int digit, is_negative = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else if (n < 0)
+	{
+		is_negative = 1;
+		n = -n;
+	}
+
+	while (n != 0)
+	{
+		digit = n % 10;
+		_putchar(digit + '0');
+		n /= 10;
+	}
+
+	if (is_negative)
+		_putchar('-');
+}
+
