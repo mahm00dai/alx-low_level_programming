b3a2d7521bc83ecac5f4f58ff49bea806ba751a0
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-08 14:17:00 -0700

    Pointers & Arrays

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  *
  * Return: A pointer to the destination memory area dest
  */
-char *_memcpy(char *dest, const char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
