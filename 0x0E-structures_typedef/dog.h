b02d48bc335460c774aba0e9c875b6b7d764ca34
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 12:18:40 -0700

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,9 @@ struct dog
 	char *owner;
 };
 
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
 
