9c126df5685597fc138be419c573fdac01c41e4b
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 10:42:52 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,35 +10,46 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i = 0;
 	char *str;
+	int is_first = 1;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
 		if (format[i] == 'c')
+		{
+			if (!is_first)
+				printf(", ");
 			printf("%c", va_arg(args, int));
+			is_first = 0;
+		}
 		else if (format[i] == 'i')
+		{
+			if (!is_first)
+				printf(", ");
 			printf("%d", va_arg(args, int));
+			is_first = 0;
+		}
 		else if (format[i] == 'f')
+		{
+			if (!is_first)
+				printf(", ");
 			printf("%f", va_arg(args, double));
+			is_first = 0;
+		}
 		else if (format[i] == 's')
 		{
+			if (!is_first)
+				printf(", ");
 			str = va_arg(args, char *);
 			printf("%s", str ? str : "(nil)");
+			is_first = 0;
 		}
-		else
-		{
-			i++;
-			continue;
-		}
-
-		if (format[i + 1] && (format[i + 1] == 'c' || format[i + 1] == 'i' ||
-			format[i + 1] == 'f' || format[i + 1] == 's'))
-			printf(", ");
 		i++;
 	}
 
 	va_end(args);
-	printf("\n");
+	if (!is_first)
+		printf("\n");
 }
 
