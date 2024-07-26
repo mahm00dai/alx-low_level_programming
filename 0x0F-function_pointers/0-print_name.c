9a9d5cc17eb4bcb47438bc31170b7d41e07fb459
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 10:10:28 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a given function
+ * @name: name of the person
+ * @f: function pointer to a function that takes a char * argument
+ *
+ * Return: Nothing.
+ */
+void	print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
+
