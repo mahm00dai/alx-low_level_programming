0060ed599205c47b2d31c892c4795f54129294b8
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 12:24:09 -0700

    3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,20 @@
 #define DOG_H
 
 /**
- * struct dog - A structure to represent a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
- *
- * Description: This structure holds information about a dog, including
- * its name, age, and owner.
+ * struct dog - Struct for storing dog information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
+
+/* Typedef for struct dog */
+typedef struct dog dog_t;
 
 /* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
