198c275e3f894f79a526347a7c0a1b88b268588b
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 10:43:57 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,46 +10,49 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i = 0;
 	char *str;
-	int is_first = 1;
+	int first = 1;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
+		int print_separator = 0;
+
 		if (format[i] == 'c')
 		{
-			if (!is_first)
-				printf(", ");
 			printf("%c", va_arg(args, int));
-			is_first = 0;
+			print_separator = 1;
 		}
-		else if (format[i] == 'i')
+
+		if (format[i] == 'i')
 		{
-			if (!is_first)
-				printf(", ");
 			printf("%d", va_arg(args, int));
-			is_first = 0;
+			print_separator = 1;
 		}
-		else if (format[i] == 'f')
+
+		if (format[i] == 'f')
 		{
-			if (!is_first)
-				printf(", ");
 			printf("%f", va_arg(args, double));
-			is_first = 0;
+			print_separator = 1;
 		}
-		else if (format[i] == 's')
+
+		if (format[i] == 's')
 		{
-			if (!is_first)
-				printf(", ");
 			str = va_arg(args, char *);
-			printf("%s", str ? str : "(nil)");
-			is_first = 0;
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+			print_separator = 1;
 		}
+
+		if (print_separator && format[i + 1] != '\0')
+			printf(", ");
+
 		i++;
 	}
 
 	va_end(args);
-	if (!is_first)
-		printf("\n");
+	printf("\n");
 }
 
