28556fd278ca6cc7c60fc6f15ced43d36f84d269
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 10:48:59 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,46 +1,77 @@
+#include <stdarg.h>
 #include "variadic_functions.h"
-#include <stdio.h>
 
 /**
- * print_all - Prints anything, followed by a new line.
- * @format: A list of types of arguments passed to the function.
+ * print_all - prints anything
+ * @format: list of types of arguments
  */
 void print_all(const char * const format, ...)
 {
     va_list args;
     unsigned int i = 0;
     char *str;
-    int first = 0;
+    int print_comma = 0;
 
     va_start(args, format);
-
     while (format && format[i])
     {
-        if (format[i] == 'c' || format[i] == 'i' ||
-            format[i] == 'f' || format[i] == 's')
+        if (print_comma)
+            _putchar(',');  /* Use _putchar to print comma */
+        print_comma = 1;
+
+        if (format[i] == 'c')
         {
-            if (first)
-                printf(", ");
-            if (format[i] == 'c')
-                printf("%c", va_arg(args, int));
-            if (format[i] == 'i')
-                printf("%d", va_arg(args, int));
-            if (format[i] == 'f')
-                printf("%f", va_arg(args, double));
-            if (format[i] == 's')
+            _putchar(va_arg(args, int));
+        }
+        if (format[i] == 'i')
+        {
+            int num = va_arg(args, int);
+            if (num < 0)
+                _putchar('-');  /* Print negative sign */
+            num = (num < 0) ? -num : num;
+            if (num == 0)
+                _putchar('0');
+            while (num > 0)
             {
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    printf("(nil)");
-                else
-                    printf("%s", str);
+                _putchar((num % 10) + '0');
+                num /= 10;
             }
-            first = 1;
+        }
+        if (format[i] == 'f')
+        {
+            float num = va_arg(args, double);  /* float promoted to double */
+            int int_part = (int)num;
+            float frac_part = num - int_part;
+            int frac_int;
+
+            if (num < 0)
+                _putchar('-');  /* Print negative sign */
+            int_part = (int_part < 0) ? -int_part : int_part;
+            while (int_part > 0)
+            {
+                _putchar((int_part % 10) + '0');
+                int_part /= 10;
+            }
+            _putchar('.');
+            frac_part *= 1000000;  /* Print 6 decimal places */
+            frac_int = (int)frac_part;
+            while (frac_int > 0)
+            {
+                _putchar((frac_int % 10) + '0');
+                frac_int /= 10;
+            }
+        }
+        if (format[i] == 's')
+        {
+            str = va_arg(args, char *);
+            if (str == NULL)
+                str = "(nil)";
+            while (*str)
+                _putchar(*str++);
         }
         i++;
     }
-
+    _putchar('\n');
     va_end(args);
-    printf("\n");
 }
 
