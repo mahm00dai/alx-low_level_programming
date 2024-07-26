44244d8396d1b16b3db35fcb0ec04524cfecdfeb
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 11:32:12 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure to represent a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This structure holds information about a dog, including
+ * its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
