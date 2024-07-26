e6bcec5f65534213aafeab87a13e0630f1ae20cd
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 10:32:42 -0700

    0. Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/* Function prototype */
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
+
