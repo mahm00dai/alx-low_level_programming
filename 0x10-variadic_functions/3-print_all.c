59693aa080827922549491e64ec9c52710cae6f5
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-07-26 10:47:00 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,52 +7,40 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *str;
-	int first = 1;
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    int first = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format && format[i])
-	{
-		int print_separator = 0;
+    while (format && format[i])
+    {
+        if (format[i] == 'c' || format[i] == 'i' ||
+            format[i] == 'f' || format[i] == 's')
+        {
+            if (first)
+                printf(", ");
+            if (format[i] == 'c')
+                printf("%c", va_arg(args, int));
+            if (format[i] == 'i')
+                printf("%d", va_arg(args, int));
+            if (format[i] == 'f')
+                printf("%f", va_arg(args, double));
+            if (format[i] == 's')
+            {
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    printf("(nil)");
+                else
+                    printf("%s", str);
+            }
+            first = 1;
+        }
+        i++;
+    }
 
-		if (format[i] == 'c')
-		{
-			printf("%c", va_arg(args, int));
-			print_separator = 1;
-		}
-
-		if (format[i] == 'i')
-		{
-			printf("%d", va_arg(args, int));
-			print_separator = 1;
-		}
-
-		if (format[i] == 'f')
-		{
-			printf("%f", va_arg(args, double));
-			print_separator = 1;
-		}
-
-		if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				printf("(nil)");
-			else
-				printf("%s", str);
-			print_separator = 1;
-		}
-
-		if (print_separator && format[i + 1] != '\0')
-			printf(", ");
-
-		i++;
-	}
-
-	va_end(args);
-	printf("\n");
+    va_end(args);
+    printf("\n");
 }
 
