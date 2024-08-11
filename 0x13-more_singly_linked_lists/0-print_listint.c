896673ea7dbdcc7e665be73f48a625313dc2e02d
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-08-11 06:44:28 -0700

    Lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * print_number - Prints an integer using _putchar.
+ * @n: The integer to print.
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
+}
+
+/**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: Pointer to the head of the list.
  *
@@ -8,11 +31,11 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		_putchar(h->n + '0');
+		print_number(h->n);
 		_putchar('\n');
 		h = h->next;
 		count++;
