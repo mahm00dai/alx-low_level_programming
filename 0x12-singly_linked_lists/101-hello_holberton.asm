18445dad0eb42d87673ccdde9ef2f58e8a0128a4
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-08-11 06:35:46 -0700

    0. Print list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,12 @@
 section .data
     msg db 'Hello, Holberton', 10, 0  ; The message to be printed, 10 is newline character, 0 is null terminator
+    format db '%s', 0       ; Format string for printf
 
 section .text
     extern printf           ; Declare the printf function
-    global _start           ; Entry point for the linker
+    global main             ; Declare main as the entry point
 
-_start:
+main:
     ; Setup parameters for printf
     mov rdi, format         ; Address of the format string
     mov rsi, msg            ; Address of the message
@@ -15,7 +16,4 @@ _start:
     mov rax, 60             ; System call number for exit (60)
     xor rdi, rdi            ; Exit code 0
     syscall                 ; Invoke the system call
-
-section .data
-    format db '%s', 0       ; Format string for printf
 
