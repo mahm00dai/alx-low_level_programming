b887a2fc87a0cde276fc3e743ffb7898c3bb96e9
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-08-11 07:05:34 -0700

    Bit Manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,10 +6,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 1)  // Check if n is greater than 1
+	if (n >> 1)
 	{
-		print_binary(n >> 1);  // Recursively call with n shifted right
+		print_binary(n >> 1);
 	}
-	_putchar((n & 1) + '0');  // Print the least significant bit (0 or 1)
+	_putchar((n & 1) + '0');
 }
 
